9d00ab71fadce68945af6c4e6e50b028d928db30
Bingdongsanchi <chrisau418@gmail.com>
2022-10-30 22:56:30 -0700

    done for the day TBC tmr

---
+++ server/api/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+router.use("/movies", require("./movies"));
+router.use("/genres", require("./genres"));
+
+
+module.exports = router;
